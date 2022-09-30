df14960e9815cea49d8cdac3d8cd2dc46a4f0a00
Grace Kostanich <fl5812597@gmail.com>
2022-09-30 11:05:58 -0700

    WIP: Connect Home and Controller components

--- skincare-and-beauty-services/src/components/AppointmentControl.js
+++ skincare-and-beauty-services/src/components/AppointmentControl.js
@@ -1,1 +1,55 @@
 //Parent
+import React, { useState } from 'react';
+import AppointmentForm from './AppointmentForm';
+import Home from './Home';
+import AppointmentList from './AppointmentList';
+import AppointmentDetail from './AppointmentDetail';
+// import { collection, addDoc, doc, updateDoc, onSnapshot, deleteDoc, query, orderBy } from "firebase/firestore";
+// import { db, auth } from './../firebase.js';
+// import { formatDistanceToNow } from 'date-fns';
+
+function AppointmentControl() {
+  //useState
+  const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
+  const [mainAppointmentList, setMainAppointmentList] = useState([]);
+  
+  //set form visible to true temporarily 
+  // setFormVisibleOnPage(true);
+  
+  let currentlyVisibleState = null;
+
+
+  const handleAddingAppointmentToList = (newAppointment) => {
+    const newMainAppointmentList = mainAppointmentList.concat(newAppointment);
+    setMainAppointmentList(newMainAppointmentList);
+    setFormVisibleOnPage(false)
+  }
+
+  // const handleAddingNewTicketToList = (newTicket) => {
+  //   // new code!
+  //   const newMainTicketList = mainTicketList.concat(newTicket);
+  //   // new code!
+  //   setMainTicketList(newMainTicketList);
+  //   setFormVisibleOnPage(false)
+  // }
+
+
+  if (formVisibleOnPage) {
+    currentlyVisibleState = 
+    <AppointmentForm 
+     onAppointmentCreation={handleAddingAppointmentToList}/>; 
+  } else {
+    currentlyVisibleState = <Home />;
+  }
+    
+
+  return (
+    <React.Fragment>
+      {currentlyVisibleState}
+    </React.Fragment>
+  );
+
+
+}  
+
+export default AppointmentControl;
---
+++ skincare-and-beauty-services/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home(){
+  return (
+    <React.Fragment>
+      <h1> HOMEPAGE </h1>
+      {/* need to add a button that sets form visible on page to true */}
+    </React.Fragment>
+  );
+}
+
+export default Home;
