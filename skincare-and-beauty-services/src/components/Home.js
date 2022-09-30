8550930c5b881d2a421094b2bd8d4ec3797c5a48
Grace Kostanich <fl5812597@gmail.com>
2022-09-30 16:37:43 -0700

    Add button that navigates to form

--- skincare-and-beauty-services/src/components/Home.js
+++ skincare-and-beauty-services/src/components/Home.js
@@ -1,12 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Home(){
+function Home(props){
+  const { handleClick } = props;
+
   return (
     <React.Fragment>
       <h1> HOMEPAGE </h1>
       {/* need to add a button that sets form visible on page to true */}
+      <button onClick={handleClick}>Make an appointment</button> 
     </React.Fragment>
   );
+}
+
+Home.propTypes = {
+  handleClick: PropTypes.func
 }
 
 export default Home;
