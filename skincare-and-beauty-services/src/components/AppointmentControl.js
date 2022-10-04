f7441a4dcf70d6e630b0bed0d43cca998531b236
Grace Kostanich <fl5812597@gmail.com>
2022-10-04 16:34:58 -0700

    Subscribe to any changes made to appointment list

--- skincare-and-beauty-services/src/components/AppointmentControl.js
+++ skincare-and-beauty-services/src/components/AppointmentControl.js
@@ -1,12 +1,12 @@
 //Parent
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppointmentForm from './AppointmentForm';
 import Home from './Home';
 import Navbar from './Navbar';
 import AppointmentList from './AppointmentList';
 import AppointmentDetail from './AppointmentDetail';
 import db from './../firebase.js'
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, onSnapshot } from "firebase/firestore";
 // import { collection, addDoc, doc, updateDoc, onSnapshot, deleteDoc, query, orderBy } from "firebase/firestore";
 // import { db, auth } from './../firebase.js';
 // import { formatDistanceToNow } from 'date-fns';
@@ -15,8 +15,40 @@ function AppointmentControl() {
   //useState
   const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
   const [mainAppointmentList, setMainAppointmentList] = useState([]);
+  // const [selectedTicket, setSelectedTicket] = useState(null);
+  const [error, setError] = useState(null);
   
   let currentlyVisibleState = null;
+
+//to listen to the list for every change made to the database
+  useEffect(() => { 
+    const unSubscribe = onSnapshot(
+      collection(db, "appointments"), 
+      (collectionSnapshot) => {
+        // do something with appointment data
+        const appointments = [];
+        collectionSnapshot.forEach((doc) => {
+            appointments.push({
+              firstName: doc.data().firstName, 
+              lastName: doc.data().lastName, 
+              serviceType: doc.data().serviceType, 
+              date: doc.data().date, 
+              time: doc.data().time, 
+              additionalInfo: doc.data().additionalInfo,
+              id: doc.id
+            });
+        });
+        setMainAppointmentList(appointments);
+      }, 
+      (error) => {
+        setError(error.message); //have error handling on list component if list fails to GET
+      }
+    );
+
+    return () => unSubscribe();
+  }, []);
+
+  console.log(mainAppointmentList);
 
 
   const handleClick = () => {
@@ -36,7 +68,9 @@ function AppointmentControl() {
   }
 
 
-  if (formVisibleOnPage) {
+  if (error) {
+    currentlyVisibleState = <p>There was an error: {error}</p>
+  } else if (formVisibleOnPage) {
     currentlyVisibleState = 
       <AppointmentForm 
         handleClick={handleClick}
