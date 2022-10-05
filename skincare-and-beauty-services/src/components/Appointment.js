363195018ba8ba50374a6035a74569a621544d5b
Grace Kostanich <fl5812597@gmail.com>
2022-10-05 10:50:50 -0700

    Add individual appointments on html for admin page

--- skincare-and-beauty-services/src/components/Appointment.js
+++ skincare-and-beauty-services/src/components/Appointment.js
@@ -1,1 +1,32 @@
 //FOR ADMIN//FOR ADMIN ONLY
+
+import React from "react";
+import PropTypes from "prop-types";
+
+function Appointment(props){
+
+  return (
+    <React.Fragment>
+      <div onClick = {() => props.whenTicketClicked(props.id)}>
+        <h3>{props.firstName} {props.lastName}</h3>
+        <p>{props.date} at {props.time}</p>
+        <p><em>{props.serviceType}</em></p>
+        <p>Additional Information:</p>
+        <p>{props.additionalInfo}</p> 
+        <hr/>
+      </div>
+    </React.Fragment>
+  );
+}
+
+Appointment.propTypes = {
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+  date: PropTypes.string,
+  time: PropTypes.string,
+  serviceType: PropTypes.string,
+  additionalInfo: PropTypes.string,
+  whenAppointmentClicked: PropTypes.func
+}
+
+export default Appointment;
