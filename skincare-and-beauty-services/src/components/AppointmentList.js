9d512a234435e2e00139d92ae37e059582e5645c
Grace Kostanich <fl5812597@gmail.com>
2022-10-05 10:49:11 -0700

    Add appointment list for admin page

--- skincare-and-beauty-services/src/components/AppointmentList.js
+++ skincare-and-beauty-services/src/components/AppointmentList.js
@@ -1,1 +1,32 @@
 //FOR ADMIN ONLY
+import React from "react";
+import Appointment from "./Appointment";
+import PropTypes from "prop-types";
+
+function AppointmentList(props){
+
+  return (
+    <React.Fragment>
+      <hr/>
+      {props.appointmentList.map((appointment) =>
+        <Appointment 
+          whenAppointmentClicked={props.onAppointmentSelection}
+          firstName={appointment.firstName}
+          lastName={appointment.lastName}
+          serviceType={appointment.serviceType}
+          date={appointment.date}
+          time={appointment.time}
+          additionalInfo={appointment.additionalInfo}
+          id={appointment.id}
+          key={appointment.id}/>
+      )}
+    </React.Fragment>
+  );
+}
+
+AppointmentList.propTypes = {
+  appointmentList: PropTypes.array,
+  onAppointmentSelection: PropTypes.func
+};
+
+export default AppointmentList;
