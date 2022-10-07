f4921bc4f06489413c883bdbdacf4c7425dd15ad
Grace Kostanich <fl5812597@gmail.com>
2022-10-07 14:01:28 -0700

    Add sign in page

--- skincare-and-beauty-services/src/components/SignIn.js
+++ skincare-and-beauty-services/src/components/SignIn.js
@@ -1,1 +1,68 @@
 //FOR ADMIN ONLY
+
+import React, { useState } from "react";
+import { auth } from "./../firebase"
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { Link } from "react-router-dom";
+import * as style from "../StyleComponents"
+
+function SignIn(){  
+  const [signInSuccess, setSignInSuccess] = useState(null);
+  const [signOutSuccess, setSignOutSuccess] = useState(null);
+
+  let homeLink; 
+
+  if (auth.currentUser != null) {
+    homeLink = <Link to="/">Appointment List</Link>;
+  } else {
+    homeLink = <Link to="/">Home</Link>;
+  }
+
+  function doSignIn(event) {
+    event.preventDefault();
+    const email = event.target.signinEmail.value;
+    const password = event.target.signinPassword.value;
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setSignInSuccess(`You've successfully signed in as ${userCredential.user.email}!`)
+      })
+      .catch((error) => {
+        setSignInSuccess(`There was an error signing in: ${error.message}!`)
+      });
+  }
+
+  function doSignOut() {
+    signOut(auth)
+      .then(function() {
+        setSignOutSuccess("You have successfully signed out!" );
+      }).catch(function(error) {
+        setSignOutSuccess(`There was an error signing out: ${error.message}!`);
+      });
+  }
+
+  return (
+    <React.Fragment>
+      {homeLink}
+      <h1>Sign In</h1>
+      {signInSuccess}
+      <form onSubmit={doSignIn}>
+        <input
+          type='text'
+          name='signinEmail'
+          placeholder='email' />
+        <input
+          type='password'
+          name='signinPassword'
+          placeholder='Password' />
+        <button type='submit'>Sign in</button>
+      </form>
+
+      <h1>Sign Out</h1>
+      {signOutSuccess}
+      <br />
+      <button onClick={doSignOut}>Sign out</button>
+    </React.Fragment>
+  );
+}
+
+export default SignIn
