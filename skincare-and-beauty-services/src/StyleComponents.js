5a3c0e9f71f0c8259795383966daa853aff24058
Grace Kostanich <fl5812597@gmail.com>
2022-10-07 13:57:39 -0700

    Style header and navigation bar components

---
+++ skincare-and-beauty-services/src/StyleComponents.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Header = styled.section`
+  font-family: 'Codystar', cursive;
+  font-weight: bolder;
+  text-shadow: 1px 1px white, 1px -1px white, -1px -1px white, -1px 1px white;
+  margin-top: 0px;
+  background-color: #f4acb7;
+  text-align: center;
+  padding: 20px;
+  font-size: 10rem;
+`
+
+export const Navbar = styled.section`
+  padding: 10px;
+  background-color: #ffcad4;
+`
